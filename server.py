7d9daf2befe8820ccd2e33475a6543f3e777dc28
Kenneth Reitz <me@kennethreitz.org>
2017-08-26 18:49:19 -0400

    no more wallets, since they don't get used
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import os
 
-from scraper import get_coins, get_coin, Coin
-from wallets import wallets
+from scraper import get_coins, get_coin, Coin, convert_to_decimal
 
 from flask import Flask, jsonify, render_template, request
 from flask_cache import Cache
@@ -35,11 +34,6 @@ def hello():
 
     return render_template('index.html', lbc=lbc, lbc_42=lbc_42, lbc_sc=lbc_sc, lbc_42_sc=lbc_42_sc, coins=get_coins().values())
 
-@app.route('/thanks')
-def thanks():
-    return jsonify(wallets=wallets, note='Your donations are greatly appreciated!')
-
-
 @app.route('/coins')
 def all_coins():
     return jsonify(coins=get_coins())
@@ -47,7 +41,7 @@ def all_coins():
 
 @app.route('/<coin>')
 def get_coin(coin):
-    c = Coin(coin)
+    c = Coin(coin.lower())
 
     return jsonify(coin={
         'name': c.name,
@@ -60,9 +54,9 @@ def get_coin(coin):
 
 @app.route('/<coin>/<float:n>')
 def get_value(coin, n):
-    c = Coin(coin)
+    c = Coin(coin.lower())
     return jsonify(coin={
-        'usd': c.usd * n,
+        'usd': convert_to_decimal(c.usd * n),
         'exchange_rate': c.usd
     })
 
@@ -74,7 +68,7 @@ def get_value_int(coin, n):
 
 @app.route('/<coin>/history')
 def get_history(coin):
-    c = Coin(coin)
+    c = Coin(coin.lower())
 
     q = "SELECT * from api_coin WHERE name=:coin ORDER BY date desc"
 
@@ -94,7 +88,7 @@ def get_history(coin):
     )
 
 @app.route('/<coin1>/to/<coin2>')
-def get_exchange(coin1, coin2):
+def get_exchange(coin1.lower(), coin2.lower()):
     c = Coin(coin1)
     return jsonify(coin={
         # 'name': c.name,
@@ -105,13 +99,11 @@ def get_exchange(coin1, coin2):
 
 @app.route('/<coin1>/<float:n>/to/<coin2>/')
 def get_exchange_value(coin1, coin2, n):
-    c = Coin(coin1)
-    v = c.value(coin2)
+    c = Coin(coin1.lower())
+    v = c.value(coin2.lower())
 
     return jsonify(coin={
-        # 'name': c.name,
-        # 'ticker': c.ticker,
-        'value': float(v) * n,
+        'value': convert_to_decimal(v * n),
         'value.coin': coin2,
         'exchange_rate': v
     })
@@ -119,7 +111,7 @@ def get_exchange_value(coin1, coin2, n):
 
 @app.route('/<coin1>/<int:n>/to/<coin2>/')
 def get_exchange_value_int(coin1, coin2, n):
-    return get_exchange_value(coin1, coin2, n)
+    return get_exchange_value(coin1.lower(), coin2, n)
 
 
 if __name__ == '__main__':
