0b7b2a5d45cd781fe606fa0a38c2a28806cd4e98
Kenneth Reitz <me@kennethreitz.org>
2017-08-27 07:52:19 -0400

    awesome

--- predictions.py
+++ predictions.py
@@ -85,7 +85,7 @@ def get_predictions(coin):
     for i, k in enumerate(list(d.keys())[-6:]):
         w = maya.when(f'{61*i} minutes from now')
         predictions.append({
-            'when': d.slang_time(),
+            'when': w.slang_time(),
             'timestamp': w.iso8601(),
             'usd': d[k],
         })
